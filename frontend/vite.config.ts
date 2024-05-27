7c9ced96b4d32ea26d191112a41159e7a2d0ec83
Sota Ichikawa <sota.0512.0213@gmail.com>
2024-05-27 15:44:00 +0900

    init: create remix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 3000,
-    // proxy: {
-    //   "/hello": "http://localhost:9000",
-    //   "/api": "http://localhost:9000",
-    // },
-  },
-  plugins: [react()],
-})
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
