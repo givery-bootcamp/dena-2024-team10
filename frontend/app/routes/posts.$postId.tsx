aa0d465b5653ee8bb4ec32fab11f2919fa5c67ba
shunsuke-tamura <wwwxx4869@gmail.com>
2024-05-28 16:33:34 +0900

    feat: 詳細ページ作成 (空行対応できてない)

--- frontend/app/routes/posts.$postId.tsx
+++ frontend/app/routes/posts.$postId.tsx
@@ -1,5 +1,7 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import classNames from "classnames";
+import formatDate from "utils/formatDate";
 import apiClient from "~/apiClient/apiClient";
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -9,7 +11,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
 		return json({
 			id: params.postId,
 			title: "title",
-			body: "body",
+			body: "body\n\nboooddddyyyyyy",
 			user_id: 1,
 			username: "username",
 			created_at: "2022-01-01T00:00:00.000Z",
@@ -27,10 +29,40 @@ export async function loader({ params }: LoaderFunctionArgs) {
 }
 
 export default function PostsDetails() {
+	const data = useLoaderData<typeof loader>();
+	const TimeTopic = (topic: string, time: string) => (
+		<div className={classNames("flex", "text-sm")}>
+			<p className={classNames("opacity-20", "mr-2")}>{topic}</p>
+			<p>{formatDate(time)}</p>
+		</div>
+	);
 	return (
 		<main className={classNames("mx-auto", "w-1/2")}>
-			<h1 className="text-3xl font-bold underline">投稿詳細</h1>
-			<p>投稿詳細ページです</p>
+			<h1 className={classNames("text-3xl", "my-3")}>{data.title}</h1>
+			<div className={classNames("flex", "gap-3")}>
+				{TimeTopic("作成日時", data.created_at)}
+				{TimeTopic("更新日時", data.updated_at)}
+			</div>
+			<hr className={classNames("my-3")} />
+			{data.body.split("\n").map((line, index) => (
+				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+				<p key={index} className={classNames("my-1")}>
+					{/* 空行対応できてない */}
+					{line}
+				</p>
+			))}
+			<p
+				className={classNames(
+					"bg-blue-200",
+					"p-2",
+					"ml-auto",
+					"w-fit",
+					"text-xs",
+					"rounded-md",
+				)}
+			>
+				{data.username}
+			</p>
 		</main>
 	);
 }
