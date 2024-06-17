df174c14020f1a5f3764cb9ecc4dfbb0ef6e6159
Sota Ichikawa <sota.0512.0213@gmail.com>
2024-06-17 15:54:13 +0900

    add: signin action

--- frontend/app/apiClient/output.generated.ts
+++ frontend/app/apiClient/output.generated.ts
@@ -2,34 +2,108 @@ import { makeApi, Zodios, type ZodiosOptions } from "@zodios/core";
 import { z } from "zod";
 
 const Post = z
-	.object({
-		id: z.number().int(),
-		title: z.string(),
-		body: z.string(),
-		user_id: z.number().int(),
-		username: z.string(),
-		created_at: z.string().datetime({ offset: true }),
-		updated_at: z.string().datetime({ offset: true }),
-	})
-	.passthrough();
+  .object({
+    id: z.number().int(),
+    title: z.string(),
+    body: z.string(),
+    user_id: z.number().int(),
+    username: z.string(),
+    created_at: z.string().datetime({ offset: true }),
+    updated_at: z.string().datetime({ offset: true }),
+  })
+  .passthrough();
+const signupUser_Body = z
+  .object({ username: z.string(), password: z.string() })
+  .passthrough();
+const UserResponse = z
+  .object({
+    user: z
+      .object({ id: z.number().int(), username: z.string() })
+      .passthrough(),
+  })
+  .partial()
+  .passthrough();
 
 export const schemas = {
-	Post,
+  Post,
+  signupUser_Body,
+  UserResponse,
 };
 
 const endpoints = makeApi([
-	{
-		method: "get",
-		path: "/posts",
-		alias: "getPosts",
-		description: `Retrieve a list of posts sorted by id in descending order.`,
-		requestFormat: "json",
-		response: z.array(Post),
-	},
+  {
+    method: "get",
+    path: "/posts",
+    alias: "getPosts",
+    description: `Retrieve a list of posts sorted by id in descending order.`,
+    requestFormat: "json",
+    response: z.array(Post),
+  },
+  {
+    method: "post",
+    path: "/signin",
+    alias: "signinUser",
+    description: `Authenticate a user by providing a username and password.`,
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        description: `User credentials needed for signing in`,
+        type: "Body",
+        schema: signupUser_Body,
+      },
+    ],
+    response: UserResponse,
+    errors: [
+      {
+        status: 400,
+        description: `Sign-in failed`,
+        schema: z.object({ message: z.string() }).partial().passthrough(),
+      },
+    ],
+  },
+  {
+    method: "post",
+    path: "/signout",
+    alias: "signoutUser",
+    description: `sign out a user.`,
+    requestFormat: "json",
+    response: z.void(),
+    errors: [
+      {
+        status: 400,
+        description: `Sign-out failed`,
+        schema: z.object({ message: z.string() }).partial().passthrough(),
+      },
+    ],
+  },
+  {
+    method: "post",
+    path: "/signup",
+    alias: "signupUser",
+    description: `Create a new user by providing a username and password.`,
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        description: `User credentials needed for signing up`,
+        type: "Body",
+        schema: signupUser_Body,
+      },
+    ],
+    response: UserResponse,
+    errors: [
+      {
+        status: 400,
+        description: `Sign-up failed`,
+        schema: z.object({ message: z.string() }).partial().passthrough(),
+      },
+    ],
+  },
 ]);
 
 export const api = new Zodios(endpoints);
 
 export function createApiClient(baseUrl: string, options?: ZodiosOptions) {
-	return new Zodios(baseUrl, endpoints, options);
+  return new Zodios(baseUrl, endpoints, options);
 }
--- frontend/app/routes/signin.tsx
+++ frontend/app/routes/signin.tsx
@@ -1,16 +1,34 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import classNames from "classnames";
+import { ZodError } from "zod";
+import apiClient, { API_BASE_URL } from "~/apiClient/apiClient";
+import { schemas } from "~/apiClient/output.generated";
 import AuthForm from "~/components/authForm";
 
 export async function action({ request }: ActionFunctionArgs) {
-	const formData = await request.formData();
-	const name = formData.get("name");
-	const pass = formData.get("pass");
-	console.log(name, pass);
-	return new Response("サインインしました", {
-		status: 200,
-	});
+	try {
+		const formData = await request.formData();
+		const username = formData.get("name") as string;
+		const password = formData.get("pass") as string;
+
+		const body = schemas.signupUser_Body.parse({ username, password });
+		const res = await fetch(`${API_BASE_URL}/signin`, {
+			method: "POST",
+			body: JSON.stringify(body),
+			headers: {
+				"content-type": "application/json",
+			},
+		});
+
+		return new Response(await res.text(), {
+			status: res.status,
+			headers: res.headers,
+		});
+	} catch (e) {
+		if (e instanceof ZodError) return new Response(e.message, { status: 400 });
+		if (e instanceof Error) return new Response(e.toString(), { status: 500 });
+	}
 }
 
 export default function SignIn() {
