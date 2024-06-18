6c2156c52a532649093a60857667927e9e99cbef
shunsuke-tamura <wwwxx4869@gmail.com>
2024-06-18 13:38:13 +0900

    feat: 投稿ページUI完成

--- frontend/app/routes/posts.new.tsx
+++ frontend/app/routes/posts.new.tsx
@@ -3,37 +3,40 @@ import SubmitButton from "~/components/submitButton";
 
 export default function () {
 	return (
-		<form method="post" className={classNames("w-1/2", "mx-auto", "p-2")}>
-			<label htmlFor="title" className={classNames("block")}>
-				タイトル
-			</label>
-			<input
-				type="text"
-				id="title"
-				name="title"
-				className={classNames(
-					"block",
-					"border",
-					"border-gray-400",
-					"w-full",
-					"mb-2",
-				)}
-			/>
-			<label htmlFor="content" className={classNames("block", "w-full")}>
-				内容
-			</label>
-			<textarea
-				name="content"
-				id="content"
-				className={classNames(
-					"block",
-					"border",
-					"border-gray-400",
-					"w-full",
-					"mb-4",
-				)}
-			/>
-			<SubmitButton color="primary" text="投稿" />
-		</form>
+		<main className={classNames("w-1/2", "mx-auto")}>
+			<h1 className={classNames("text-4xl", "my-4")}>新しい投稿を作成する</h1>
+			<form method="post" className={classNames("p-2")}>
+				<label htmlFor="title" className={classNames("block")}>
+					タイトル
+				</label>
+				<input
+					type="text"
+					id="title"
+					name="title"
+					className={classNames(
+						"block",
+						"border",
+						"border-gray-400",
+						"w-full",
+						"mb-2",
+					)}
+				/>
+				<label htmlFor="content" className={classNames("block", "w-full")}>
+					内容
+				</label>
+				<textarea
+					name="content"
+					id="content"
+					className={classNames(
+						"block",
+						"border",
+						"border-gray-400",
+						"w-full",
+						"mb-4",
+					)}
+				/>
+				<SubmitButton color="primary" text="投稿" />
+			</form>
+		</main>
 	);
 }
