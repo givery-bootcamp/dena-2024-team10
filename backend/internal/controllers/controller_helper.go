34ff339e83d3320c808d5192bb96d248d601e9ee
akihito-nishikawa <akihito.nishikawa@dena.jp>
2024-06-24 11:36:58 +0900

    delete: unused helper

--- backend/internal/controllers/controller_helper.go
+++ backend/internal/controllers/controller_helper.go
@@ -5,20 +5,7 @@ import (
 	"gorm.io/gorm"
 )
 
-// TODO: delete after implementing error handling to post_controller.go
-type ErrorResponse struct {
-	Message string "json:`message`"
-}
-
 // TODO: delete after implementing dependency injection
 func DB(ctx *gin.Context) *gorm.DB {
 	return ctx.MustGet("db").(*gorm.DB)
-}
-
-// TODO: delete after implementing error handling to post_controller.go
-func handleError(ctx *gin.Context, status int, err error) {
-	res := ErrorResponse{
-		Message: err.Error(),
-	}
-	ctx.JSON(status, &res)
 }
