0a3ae6bf68069444deac7a6d515c8f5b0c6b4c80
akihito-nishikawa <akihito.nishikawa@dena.jp>
2024-06-24 18:10:54 +0900

    refactor: change status code

--- backend/internal/exception/exception.go
+++ backend/internal/exception/exception.go
@@ -20,8 +20,8 @@ var (
 	ErrDuplicateUser  = new(http.StatusBadRequest, "Duplicate User")
 	ErrInvalidQuery   = new(http.StatusBadRequest, "Invalid Query")
 	ErrInvalidRequest = new(http.StatusBadRequest, "Invalid Request")
-	ErrSigninFailed   = new(http.StatusBadRequest, "Signin Failed")
 	ErrUnauthorized   = new(http.StatusUnauthorized, "Unauthorized")
+	ErrSigninFailed   = new(http.StatusUnauthorized, "Signin Failed")
 	ErrNotFound       = new(http.StatusNotFound, "Not Found")
 	// 5xx: Server Error
 	ErrInternalServerError = new(500, "Internal Server Error")
