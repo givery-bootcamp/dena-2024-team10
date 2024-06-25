71e1003855c856e3f29eddca40012a3d8f805f8d
shunsuke-tamura <wwwxx4869@gmail.com>
2024-06-25 18:53:20 +0900

    rename: なんか挙動おかしかったので、リネーム(予約語とかなんかな)

--- frontend/app/components/baseInternalError.tsx
+++ frontend/app/components/baseInternalError.tsx
@@ -4,15 +4,15 @@ import classNames from "classnames";
 export default function InternalError({
 	title,
 	to,
-	toMessage,
-}: { title: string; to: string; toMessage: string }) {
+	toPageName,
+}: { title: string; to: string; toPageName: string }) {
 	return (
 		<main className={classNames("w-1/2", "mx-auto")}>
 			<h1 className={classNames("text-4xl", "my-4")}>{title}</h1>
 			<p>しばらく待ってからやり直してください</p>
 			<div className={classNames("text-end", "mr-5")}>
 				<Link to={to} className={classNames("text-blue-600", "underline")}>
-					{toMessage}
+					{toPageName}画面へ
 				</Link>
 			</div>
 		</main>
