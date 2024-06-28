35a96cb6d1c333a8d70ce9b7bfa88d926603a4b4
shunsuke-tamura <wwwxx4869@gmail.com>
2024-06-28 11:54:44 +0900

    feat: rootのエラーハンドリングをリッチにした

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,10 +1,16 @@
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import type {
+	ErrorResponse,
+	LinksFunction,
+	LoaderFunctionArgs,
+} from "@remix-run/node";
 import {
+	Link,
 	Links,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
+	isRouteErrorResponse,
 	redirect,
 	useLoaderData,
 	useRouteError,
@@ -66,11 +72,39 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export function ErrorBoundary() {
 	const error = useRouteError();
-	console.error(error);
+	type MyRouteError = {
+		type: "MyRouteError";
+		status: number;
+		data: string;
+		string: string;
+	};
+	const errorResponseToMyRouteError = (error: ErrorResponse): MyRouteError => {
+		const { status, data } = error;
+		return {
+			type: "MyRouteError",
+			status,
+			data,
+			string: JSON.stringify(error),
+		};
+	};
+	let title = "Oops!";
+	let message = "Something went wrong!";
+	const toMessage = "Go to Top Page";
+	const toLink = "/";
+
+	if (isRouteErrorResponse(error)) {
+		if (error.status === 404) {
+			title = "Page Not Found";
+			message = "Sorry, we couldn't find the page you're looking for.";
+		}
+		console.error(JSON.stringify(errorResponseToMyRouteError(error)));
+	} else {
+		console.error(JSON.stringify(error));
+	}
 	return (
 		<html lang="ja">
 			<head>
-				<title>Oh no!</title>
+				<title>{title}</title>
 				<Meta />
 				<Links />
 			</head>
@@ -85,8 +119,20 @@ export function ErrorBoundary() {
 						"mt-8",
 					)}
 				>
-					Oh no!
+					{title}
 				</h1>
+				<p className={classNames("text-center", "mt-4")}>{message}</p>
+				<p
+					className={classNames(
+						"text-center",
+						"mt-4",
+						"text-blue-600",
+						"underline",
+						"hover:text-blue-300",
+					)}
+				>
+					<Link to={toLink}>{toMessage}</Link>
+				</p>
 				<Scripts />
 			</body>
 		</html>
