e7ae5f6c1b1c9ac3ec3b1ece844899c1f99bcc5b
Ryo Natori <ryo.natori@dena.com>
2024-06-28 10:40:04 +0900

    Update backend/internal/usecases/update_post_usecase.go
    Co-authored-by: Akihito Nishikawa <63628739+aki-nishikawa@users.noreply.github.com>

--- backend/internal/usecases/update_post_usecase.go
+++ backend/internal/usecases/update_post_usecase.go
@@ -22,10 +22,10 @@ func NewUpdatePostUsecase(
 
 func (u *UpdatePostUsecase) Execute(request schema.PostRequest, userId int64, postId int64) (*entities.Post, error) {
 	post, err := u.postRepository.GetById(postId)
-	if post == nil || err == errors.New("post not found") {
-		return nil, exception.ErrPostNotFound
-	} else if err != nil {
+	if err != nil {
 		return nil, err
+	} else if post == nil {
+	        return nil, exception.ErrPostNotFound
 	}
 
 	// Check if the user is authorized to delete the post
